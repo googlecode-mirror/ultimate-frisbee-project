5f982f0a87c6bdc73d878d39be97ab1115255964
adrianritomsky <adrianritomsky@gmail.com>
2012-12-14 15:23:05 +0000

    [No log message]

--- js/main.js
+++ js/main.js
@@ -5,66 +5,72 @@ $(document).ready(function(){
 
 function Start()
 {
-  $('#pridavanie').hide();
-  $('#vyberTimov').hide();
-  $('#pridavaniePohybu').hide();
-   $('#form1').hide();
-   $('#loadNastavenia').hide();
-   $('#start').hide();
+   $('.nastavenia').hide();
    $('#opatovnyStart').hide();
    
-   $('#opatovnyStart').unbind('click').click(function(){ 
-    animation.run();
+   $('#opatovnyStart').unbind('click').click(function(){
+        animation.run();
    });
-  
-  $('#tlacidlo').html('EDIT').unbind('click').click(function(){
-  $('#load').hide(); 
-  $('#tlacidlo').hide();   
-  $('#vyberTimov').show();
-   $('#form1').show();
+    
+   $('#start').click(function() {
+        animation.run();
+   });
+   
+  $('#save').click(function(){
+      var skuska = animation.skuska();
+      document.getElementById("save").value = skuska;
+      document.getElementById("pom").value = document.getElementById("textSave").value + ".txt";
+      $("#loadNazvy").append("<p>" + document.getElementById("pom").value + "</p>");
   });
-  
-  $('#prvyTim').unbind('click').click(function(){ 
+
+  $('#load').click(function(){
+      $('.nastavenia').hide();
+      $('#loadNastavenia').show();
+  });   
+ 
+  $('#edit').unbind('click').click(function(){
+      $('.nastavenia').hide();
+     $('#pridavanie').hide();
+     $('#pridavaniePohybu').hide();
+     $('#editNastavenia').show(); 
+     $('#vyberTimov').show();
+     $('#form1').hide();
+     $('#tlacidlo').hide();
+  });
+ 
+  $('#prvyTim').unbind('click').click(function(){
   if($('#dlzkaCyklu').val() > 1999) {
       $('#tlacidlo').show();
       $('#vyberTimov').hide();
-      $('#form1').hide();
-  
       animation.pos();
       animation.hrac("blue",$('#dlzkaCyklu').val());  
     } else {
      alert("Zadal si zly vstup na dlzke animacie! Dlzka musi byt vacsia ako 2000!")
     }
   });
-  
+ 
   $('#druhyTim').unbind('click').click(function(){
   if($('#dlzkaCyklu').val() > 1999) {
       $('#tlacidlo').show();
       $('#vyberTimov').hide();
-      $('#form1').hide();
-  
       animation.pos();
       animation.hrac("red",$('#dlzkaCyklu').val());  
     } else {
      alert("Zadal si zly vstup na dlzke animacie! Dlzka musi byt vacsia ako 2000!")
     }
   });
-  
-  $('#lopta').unbind('click').click(function(){ 
+ 
+  $('#lopta').unbind('click').click(function(){
   if($('#dlzkaCyklu').val() > 1999) {
       $('#tlacidlo').show();
       $('#vyberTimov').hide();
-      $('#form1').hide();
-  
       animation.pos();
       animation.hrac("white",$('#dlzkaCyklu').val());  
     } else {
      alert("Zadal si zly vstup na dlzke animacie! Dlzka musi byt vacsia ako 2000!")
     }
   });
-  
-  
+ 
+ 
 
 }
-
-
